b92a9ff1a6eeb3d7989b59674c5d86a6fbdafd31
zaliubovskiy <77985743+zaliubovskiy@users.noreply.github.com>
2021-02-04 12:44:56 +0200

    Delete 01_zadacha.py

--- 01_zadacha.py
+++
@@ -1,8 +0,0 @@
-#Пользователь вводит два числа. Найдите сумму и произдеведение данных чисел.
-
-first_number = int(input("Введите первое число:"))
-second_number = int(input("Введите второе число:"))
-
-print(f"This is sum of numbers: {first_number + second_number}")
-print(f"This is multiplication of numbers: {first_number * second_number}")
-
