8d79425f8348336e577bff5e6303c0e72dc51a85
Alex Leon <leon740gk@gmail.com>
2021-02-04 23:36:45 +0200

    Update HomeTask_07.py
    Answer the question

--- HomeWork_01_28.01.2021/HomeTask_07.py
+++ HomeWork_01_28.01.2021/HomeTask_07.py
@@ -2,6 +2,23 @@
 # У пользователя три попытки отгадать.
 # После каждой неудачной попытки компьютер сообщает меньше или больше загаданное число.
 
+# Randomly select a number in range from 1 to 100
+# from random import randint
+# computer_number = randint(1, 100)
+# for i in range(3):
+#     user_number = int(input("Guess number: "))
+#     if user_number == computer_number:
+#         print(f"Nice one! It is really {computer_number}")
+#         break
+#     elif user_number < computer_number:
+#         print("Your number is a bit lesser, try to increase")
+#     else:
+#         print("Your number is a bit bigger, try to decrease")
+# else:
+#     print("Unfortunately, you loose")
+#     print(f"number was --> {computer_number}")
+
+
 from random import randint
 
 a = randint(1, 100)  # Randomly select a number in range from 1 to 100
